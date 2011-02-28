2a108eb8b20783f249bc16cc37902c3da6acddcb
Michael Huynh <mike.huynh@gmail.com>
2011-02-28 12:28:08 -0500

    Refactored script to not rely on external template and removed duplicate code.

--- macros/tafel_gen_current.py
+++ macros/tafel_gen_current.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-import re #for sub
 from string import Template
 import numpy #for arange
 
@@ -11,44 +9,45 @@ end_current = -2.0 #this is log() value
 #step = 0.25 #this is log() value
 step = 0.50 #this is log() value
 each_runtime = 300 #seconds; runtime of each datapoint
-initial_template_path = 'tafel_commented.mcr'
+num_passes = 2 #number of duplicate Tafel runs. Starts at 1
 
-#Open template file and remove all comments (since electrochemical workstation
-#can't handle them).
-f = open(initial_template_path)
-initial_template = f.read()
-f.close()
-initial_template = re.sub(r'\s*#.+\n', '\n', initial_template)
-
-print '\xeb\x05\x00\x00'
+#Templates
+#CP technique settings
+header_template = (
+'tech: cp',      #CP = chronopotentiometry
+'pn = p',        #initial current polarity in CP
+'si = 1',        #sample interval in BE, CP (in s)
+'cl = 1',        #number of segments in CV and CP
+'priot',         #time priority in CP
+)
+header_template = '\n'.join(header_template)
 
 #Change `ta` to the time we want for the run. 
 #ta = 600 is a pretty good default.
-template = Template('''
-ia = %0.2e
-ta = $runtime
+run_template = Template('''
+ia = ${current}
+ta = ${runtime}
 run
-save: cp%d''')
-template = template.substitute(runtime = each_runtime)
+save: cp${run}_${pass_count}''')
+#run_template = run_template.substitute(runtime = each_runtime)
 
-print initial_template
+#Electrochemical workstation is dumb and wants a binary COM file. Any other sane
+#file format will not be read. So this little binary tidbit is for specifying
+#the COM file beginning.
+print '\xeb\x05\x00\x00'
 
 #We add step to the end_current since arange does not end at our specified
 #end_current, but rather one step before it.
-range = numpy.arange(start_current, end_current + step, step)
-for i, v in enumerate(range):
-    #Convert from log(v) to v
-    v = 10**v
-    i += 1 #start counter at 1 instead of 0
-    print template % (v, i)
-
-#Now we have to do this again, but for the second run
-print 'delay = 30' #short delay before doing again
-print initial_template
-
-template += '_2' #make the save file be filename_2
-for i, v in enumerate(range):
-    #Convert from log(v) to v
-    v = 10**v
-    i += 1 #start counter at 1 instead of 0
-    print template % (v, i)
+current_range = numpy.arange(start_current, end_current + step, step)
+for pass_count in range(1, num_passes + 1): #shift range to start at 1
+    print header_template
+    for i, v in enumerate(current_range):
+        #Convert from log(v) to v
+        v = 10**v
+        i += 1 #start counter at 1 instead of 0
+        #Format v in scientific mode
+        v = '%0.2e' % v
+        print run_template.substitute(current = v, run = i, 
+                pass_count = pass_count, runtime = each_runtime)
+    print
+    print
