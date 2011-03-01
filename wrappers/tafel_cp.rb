b8d49e9475244a2b02856d8ada23c7aa31d6c6ac
Michael Huynh <mike.huynh@gmail.com>
2011-02-28 20:35:48 -0500

    Adding crude tafel points generator.

---
+++ wrappers/tafel_cp.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+# configuration
+datafiles = 'cp%d.txt'
+data_start = 1
+data_end = 11
+
+for i in data_start..data_end do
+  cpfile = datafiles % i #generate filename of the current cp file.
+
+  ##Get the anodic current for the run
+  f = File.open(cpfile)
+  f.each do |line|
+    #Look for the anodic current line
+    #ie. Anodic Current (A) = 1e-7
+    if line =~ /Anodic Current \(A\) = ([\d.e-]+)\s*/
+        $current = $1
+        break
+    end
+  end
+  #Since we want a Tafel plot, take the log of the current:
+  $logcurrent = Math.log10($current.to_f)
+
+  ## Get the potential for the run
+  #Get the last line of the file (which we are assuming is where equilibrium 
+  #occurs).
+  cmd = 'tail -n1 %s' % cpfile
+  last_pt = `#{cmd}`
+  #puts last_pt
+
+  $potential = last_pt.split(',').last.strip()
+  #Convert from scientific string to floating pt number.
+  $potential = $potential.to_f 
+    
+  puts '%e, %e' % [$logcurrent, $potential]
+
+end
