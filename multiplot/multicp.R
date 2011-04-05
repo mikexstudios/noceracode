87d272675c4188ca024fa8b89d32d0ab38754a98
Michael Huynh <mike.huynh@gmail.com>
2011-04-05 17:24:47 -0400

    Adjusted multicp.R to accept a commandline arg denoting the pass.

--- multiplot/multicp.R
+++ multiplot/multicp.R
@@ -1,18 +1,25 @@
 #!/usr/bin/env Rscript
 
-pdf('multicp_2.pdf')
+args <- commandArgs(TRUE)
+n <- as.integer(args[1])
+
+input <- sprintf('cp%s_%d.txt', '%d', n)
+output <- sprintf('multicp_%d.pdf', n)
+
+pdf(output)
 
 par(mfrow = c(3,3)) #row, col
 
 for(i in seq(1, 13)) {
-     csv <- sprintf("cp%d_2.txt", i)
+     csv <- sprintf(input, i)
      cp <- read.csv(file = csv, skip = 18)
      #Because the second plot is smaller, we need to adjust the y-axis range here.
      plot(cp$Time.sec, cp$Potential.V,
           main = csv,
           xlab = "t (s)", ylab = "E (V)",
           #type = "l",
-          cex = 0.4 #size of the points
+          cex = 0.4, #size of the points
+          #ylim = c(0.85, 1.31)
           )
 }
 
