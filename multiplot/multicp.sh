200bb3a1e6fa3e16765b3ebc3d4e4f1d32af6b6e
Michael Huynh <mike.huynh@gmail.com>
2011-04-05 17:46:32 -0400

    Adding quick shell script for multicp plots.

---
+++ multiplot/multicp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./multicp.R 1 && open multicp_1.pdf
+./multicp.R 2 && open multicp_2.pdf
+./multicp.R 3 && open multicp_3.pdf
+./multicp.R 4 && open multicp_4.pdf
+./multicp.R 5 && open multicp_5.pdf
+./multicp.R 6 && open multicp_6.pdf
