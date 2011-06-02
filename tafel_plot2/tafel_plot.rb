4f1e24972f30a1ab81ace02e52bf133974f1d08b
Michael Huynh <mike.huynh@gmail.com>
2011-06-02 19:21:36 -0400

    Added output of linear model slope, intercept, and R^2

--- tafel_plot2/tafel_plot.rb
+++ tafel_plot2/tafel_plot.rb
@@ -46,7 +46,10 @@ class TafelPlot
     @r.assign('x', @x)
     @r.assign('y', @y)
 
-    fit = @r.lm('y ~ x') #linear model of y = m*x + b
+    #fit = @r.lm('y ~ x') #linear model of y = m*x + b
+    #We use the below instead of above to assign the output to fit and get the
+    #fit object all at once.
+    fit = @r.eval_R("fit <- lm('y ~ x')")
     coefficients = fit['coefficients']
     coefficients = { :slope => coefficients['x'], 
                      :intercept => coefficients['(Intercept)'] }
@@ -55,6 +58,36 @@ class TafelPlot
     #need to mirror that by attaching a hash at the end.
     @r.abline(coefficients[:intercept], coefficients[:slope], 
               { :col => @fit_color })
+
+    #Also print formatted output of coefficients and R^2 values
+    summary = @r.eval_R('summary(fit)')
+
+    #Transform the glob of mess that we get back into something more meaningful
+    r_squared = summary['r.squared']
+
+    coefficients = summary['coefficients']
+    coefficients = { :slope => coefficients.last, 
+                     :intercept => coefficients.first }
+    temp = {}
+    coefficients.keys.each do |key|
+      temp[key] = {
+        :estimate => coefficients[key][0],
+        :std_error => coefficients[key][1],
+        :t_value => coefficients[key][2],
+        :pr => coefficients[key][3],
+      }
+    end
+    coefficients = temp
+
+    #Now out the values
+    puts 'Slope:     %6.2f mV +/- %6.2f mV' % [coefficients[:slope][:estimate] * 1000, 
+                                   coefficients[:slope][:std_error] * 1000]
+    puts 'Intercept: %6.2f  V +/- %6.2f  V' % [coefficients[:intercept][:estimate],
+                                   coefficients[:intercept][:std_error]]
+    puts 'R^2:       %6.2f' % [r_squared * 100]
+    puts
+
+
   end
 
   def draw
