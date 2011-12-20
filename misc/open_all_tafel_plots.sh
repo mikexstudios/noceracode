74f13e83ed1b27597471d461c867802e2c6f319d
Michael Huynh <mike.huynh@gmail.com>
2011-12-20 15:38:20 -0500

    Added helper script to print multi-page tafel plots.

---
+++ misc/open_all_tafel_plots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# When run inside a directory, recursively searches sub-directories for:
+# 1. tafel_combined.pdf and tafel_#.pdf files
+# 2. tafel_combined_fit.pdf files
+# These files are opened all in one Preview session to make it easy to print
+# multiple plots one one page.
+
+#This first line finds all files of either tafel_combined.pdf or tafel_#.pdf.
+#Then it excludes (using the !) the old/ and combined/ paths.
+find -E . \( -regex '.+/tafel_combined.pdf' -o -regex '.+/tafel_[0-9]+.pdf' \) \! -path './old/*' \! -path './combined/*' \
+#We pass the multi-line output into xargs to combine it all into one line
+| xargs \
+#We then pass the whole line into open to invoke Preview
+| xargs open
