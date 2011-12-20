e36044f01e2dc70dfe5f9bd09195661992f23218
Michael Huynh <mike.huynh@gmail.com>
2011-12-20 15:41:44 -0500

    Added tafel_combined_fit.pdf gathering.

--- misc/open_all_tafel_plots.sh
+++ misc/open_all_tafel_plots.sh
@@ -7,8 +7,12 @@
 
 #This first line finds all files of either tafel_combined.pdf or tafel_#.pdf.
 #Then it excludes (using the !) the old/ and combined/ paths.
-find -E . \( -regex '.+/tafel_combined.pdf' -o -regex '.+/tafel_[0-9]+.pdf' \) \! -path './old/*' \! -path './combined/*' \
 #We pass the multi-line output into xargs to combine it all into one line
-| xargs \
 #We then pass the whole line into open to invoke Preview
+find -E . \( -regex '.+/tafel_combined.pdf' -o -regex '.+/tafel_[0-9]+.pdf' \) \! -path './old/*' \! -path './combined/*' \
+| xargs \
+| xargs open
+
+find -E . -regex '.+/tafel_combined_fit.pdf' \! -path './old/*' \! -path './combined/*' \
+| xargs \
 | xargs open
