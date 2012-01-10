46d2ef0b8e3371f61f8f3ae9aabdf5e9478f0326
Michael Huynh <mike.huynh@gmail.com>
2012-01-09 22:37:16 -0500

    Checking in temporary work on adding overpotentials to plotting script.

--- tafel_plot/tafel_plot.R
+++ tafel_plot/tafel_plot.R
@@ -1,14 +1,19 @@
 #!/usr/bin/env Rscript
 
 pdf('tafel.pdf')
-t1 <- read.csv(file='test.plot', header = FALSE)
+t1 <- read.csv(file='tafel_1.csv', header = FALSE)
+#Add extra margin to the right side of the plot
+par(mar=c(4,4,4,4))
 plot(t1$V1, t1$V2, main="Tafel plot", xlab="log(i)", ylab="E (V)")
 
+axis(4, at = axTicks(2), label = axTicks(2) - 0.6)
+mtext("eta (V)", side=4, line=2)
+
 #We only want to select a subset ot points
-lt1 <- t1[2:9,c('V1','V2')]
-fit <- lm(lt1$V2 ~ lt1$V1)
-abline(fit, col='red')
-summary(fit)
+#lt1 <- t1[2:9,c('V1','V2')]
+#fit <- lm(lt1$V2 ~ lt1$V1)
+#abline(fit, col='red')
+#summary(fit)
 
 #Flush to file
 dev.off()
