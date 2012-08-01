ce8f5d9f8b7bcc4feb5bccdb2c21a8297f3833bc
Michael Huynh <mike.huynh@gmail.com>
2012-08-01 10:31:44 -0400

    Added attempt at rotating label in tafel.r. Unsuccessful though.

--- rplothelper/R/tafel.r
+++ rplothelper/R/tafel.r
@@ -74,7 +74,6 @@ plot.tafel.legend <- function(..., lwd = 6, cex = 2.5, seg.len = 3) {
 plot.tafel.add_overpotentials <- function(E_water_ox_standard) {
     #NOTE: User must override plot.tafel.setup with extra margin on
     #      the right side of the plot!
-
     axis(4, at = axTicks(2), 
          tck = 0.05,
          lwd = 4,
@@ -83,8 +82,30 @@ plot.tafel.add_overpotentials <- function(E_water_ox_standard) {
          label = round((axTicks(2) - E_water_ox_standard) * 1000)) 
     #Use modded minor.tick to add ticks to right axis.
     minor.tick(nx=2, ny=2, tick.ratio=-2, side = 4, lwd = 3)
-    mtext(expression(eta ~ "(mV)"), side = 4, line = 5.5, cex = 3.0)
+
+    mtext(expression(eta ~ "(mV)"), side = 4, line = 5.3, cex = 3.0)
+    
+    #The problem with mtext is that we can't rotate that label. Thus,
+    #manually calculate the position of the mtext
+    #Get plot coordinates and find the middle coordinate of the right axis.
+    #plot_coords = par('usr', no.readonly = TRUE)
+    #plot.width.x = plot_coords[2] - plot_coords[1]
+    #plot.width.y = plot_coords[4] - plot_coords[3]
+    #x2 = plot_coords[2]
+    #y_center = plot.width.y/2
+    ##Get typical string line height
+    #line_height = (par('mai')[1]/par('mar')[1])/strheight('test','inches', cex = 3.0)
+    ##line = 5.5
+    #line = -0.5
+    #y_center = 1.1
+    #print(par('usr'))
+    #text(x = x2 + (0.5 + line) * line_height, y = y_center, 
+    #     expression(eta ~ "(mV)"),
+    #     cex = 3.0,
+    #     srt = -90)
+
 }
+
 
 
 tafel.linear_fit <- function(iac.log, potential, range = TRUE, color = 'black')
